a7864a3bf3b398bdbc66dea2079f83f4856bea5c
fassahat <idfassahata@gmail.com>
2026-04-10 13:19:31 +0200

    Client config.ts: remove hardcoded LOCAL_IP

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,7 +1,6 @@
 import { Platform } from "react-native";
 
-// const LOCAL_IP = Platform.OS === "android" ? "10.0.2.2" : "localhost";
-const LOCAL_IP = '192.168.178.34'
+const LOCAL_IP = Platform.OS === "android" ? "10.0.2.2" : "localhost";
 
 export const API_BASE_URL = `http://${LOCAL_IP}:8000`;
 export const WS_BASE_URL = `ws://${LOCAL_IP}:8000`;
